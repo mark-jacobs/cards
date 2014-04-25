9d7ea689949be6a8b8ace3629bb42d94bfec104d
mark-jacobs <your_email@example.com>
2014-04-25 22:44:43 +0100

    Added extra methods for deck

--- cards.rb
+++ cards.rb
@@ -6,6 +6,10 @@ class Card
     @suit = suit
     @rank = rank
   end
+
+  def display_card
+    puts "#{@rank} of #{@suit}"
+  end
 end
 
 # Deck of 52 playing cards.
@@ -15,47 +19,48 @@ class Deck
     @deck_array = []
     @card_suit
     @card_rank
+    @card_counter = 52
     # Loops to generate cards and populate the deck.
     4.times do |suit|
       13.times do |rank|
         # Set the suit
         case suit
-        when 1
+        when 0
           @card_suit = :spades
-        when 2 
+        when 1 
           @card_suit = :hearts
-        when 3
+        when 2
           @card_suit = :diamonds
-        when 4
+        when 3
           @card_suit = :clubs
         end
         # Set the rank
         case rank
-        when 1
+        when 0
           @card_rank = :ace 
-        when 2
+        when 1
           @card_rank = :two
-        when 3 
+        when 2 
           @card_rank = :three
-        when 4 
+        when 3 
           @card_rank = :four
-        when 5
+        when 4
           @card_rank = :five
-        when 6
+        when 5
           @card_rank = :six
-        when 7
+        when 6
           @card_rank = :seven
-        when 8
+        when 7
           @card_rank = :eight
-        when 9
+        when 8
           @card_rank = :nine
-        when 10
+        when 9
           @card_rank = :ten
-        when 11
+        when 10
           @card_rank = :jack
-        when 12
+        when 11
           @card_rank = :queen
-        when 13
+        when 12
           @card_rank = :king
         end
         # Create the card and add to the array.
@@ -63,10 +68,26 @@ class Deck
       end
     end
   end
+
+  # Display each card in the deck, for testing.
+  def display_deck
+    @deck_array.each do |x|
+      x.display_card
+    end
+  end
+
+  # Deal a card.
+  def deal_card
+    @card_counter -= 1
+    return card_to_return = @deck_array[@card_counter]
+  end
 end
 
-my_deck = Deck.new
 
+my_deck = Deck.new
+#my_deck.display_deck
+my_deck.deal_card.display_card
+#y.display_card
 
 
 
